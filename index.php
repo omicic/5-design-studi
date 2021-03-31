8b7f2f83153ac698db0347caa6c75223d7daa5db
Olivera Micic <omicic@gmail.com>
2021-03-31 10:12:06 +0200

    First commit, navigacija

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Design studio</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <h1>Creative</h1>
+            <ul>
+                <li><a href="index.php">home</a></li>
+                <li><a href="index.php">team</a></li>
+                <li><a href="index.php">projects</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
